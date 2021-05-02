823adf54faf7c649a2b02f797f5cd181956b6b31
munishaboora <78922862+munishaboora@users.noreply.github.com>
2021-05-02 16:52:04 +0100

    Dashboard wiht edited sidebar

--- ecogain-frontend/src/components/RegisterForm/RegisterForm.js
+++ ecogain-frontend/src/components/RegisterForm/RegisterForm.js
@@ -11,21 +11,21 @@ const RegisterForm = () => {
     const [passwordTwo, setPasswordTwo] = useState('')
 
     const history = useHistory()
-    
+
 
     const makeNewUser = async (user) => {
         let resBody = JSON.stringify(user);
         console.log(resBody)
         const res = await fetch("http://localhost:5000/user", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: resBody,
-    }); 
-    const data = await res.json()
-    console.log(data)
-    history.push('/')
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: resBody,
+        });
+        const data = await res.json()
+        console.log(data)
+        history.push('/')
     }
 
 
@@ -33,7 +33,7 @@ const RegisterForm = () => {
         e.preventDefault()
         // can do validation on password == password 2
         // call func to do POST request to make new user
-        makeNewUser({name,username,email,passwordOne})
+        makeNewUser({ name, username, email, passwordOne })
 
     }
 
@@ -42,26 +42,26 @@ const RegisterForm = () => {
         <div>
             <form onSubmit={submitRegisterForm}>
                 <div class="input-group">
-                    <label>Name</label>
-                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="What's your name?"/>
+                    <label>Full name</label>
+                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="What's your name?" />
                 </div>
                 <div class="input-group">
                     <label>Username</label>
-                    <input value={username} onChange={(e) => setUsername(e.target.value)}  type="text" placeholder="PLease enter a username"/>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="PLease enter a username" />
                 </div>
                 <div class="input-group">
                     <label>Email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="What's your email?"/>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder="What's your email?" />
                 </div>
                 <div class="input-group">
                     <label>Password</label>
-                    <input value={passwordOne} onChange={(e) => setPasswordOne(e.target.value)} type="text" placeholder="PLease enter a password"/>
+                    <input value={passwordOne} onChange={(e) => setPasswordOne(e.target.value)} type="text" placeholder="PLease enter a password" />
                 </div>
                 <div class="input-group">
-                    <label>Repeat password</label>
-                    <input value={passwordTwo} onChange={(e) => setPasswordTwo(e.target.value)} type="text" placeholder="Please repeat your password"/>
+                    <label>Confirm Password</label>
+                    <input value={passwordTwo} onChange={(e) => setPasswordTwo(e.target.value)} type="text" placeholder="Please repeat your password" />
                 </div>
-                <input type="submit" value="Register now!" />
+                <input type="submit" value="Register now" />
             </form>
         </div>
     )
--- ecogain-frontend/src/components/UserDashboard/UserDashboard.js
+++ ecogain-frontend/src/components/UserDashboard/UserDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './UserDashboard.css';
-import { Layout, Avatar, Menu, Progress, Row, Col, Divider } from 'antd';
-import { UserOutlined, MenuOutlined, MailOutlined, SettingOutlined, AppstoreOutlined, StarTwoTone } from '@ant-design/icons';
+import { Layout, Avatar, Menu, Progress, Row, Col, Popover } from 'antd';
+import { UserOutlined, MenuOutlined, MenuUnfoldOutlined, MenuFoldOutlined, MailOutlined, SettingOutlined, AppstoreOutlined, StarTwoTone } from '@ant-design/icons';
 import Title from 'antd/lib/typography/Title';
 import SubMenu from 'antd/lib/menu/SubMenu';
 import transportation from '../Images/transportation.svg';
@@ -16,84 +16,101 @@ import tshirt from '../Images/tshirt.svg';
 import reuse from '../Images/reuse.svg';
 const { Header, Footer, Sider, Content } = Layout;
 
+class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
 
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
+  render() {
+    return (
+      <div className="App">
         <Layout>
-          <Header style={{ padding: 13, backgroundColor: '#449BCD' }} >
-            <Title style={{ color: 'white' }} level={2}>EcoGain <MenuOutlined style={{ float: 'right' }} />
-            </Title>
-          </Header>
+          <Layout>
+            <Header className="site-layout-background" style={{ padding: 9, backgroundColor: '#449BCD' }} >
 
-          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+              <Title style={{ color: 'white' }} level={2}>
+                <h1 style={{ color: 'white', alignItems: "center" }}>
+                  EcoGain
+                   {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                  className: 'trigger',
+                  onClick: this.toggle
+                })}</h1>
+              </Title>
+            </Header>
+
+            <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
 
 
-            <div className="site-layout-background" style={{ minHeight: 380 }}>
-              <div>
-                <StarTwoTone spin style={{ float: 'left', fontSize: '30px' }} /> <Progress percent={55} status="active" />
+              <div className="site-layout-background" style={{ minHeight: 380 }}>
+                <div>
+                  <StarTwoTone spin style={{ float: 'left', fontSize: '30px' }} /> <Progress percent={55} status="active" />
+                </div>
+                <div>
+                  <Row justify="space-around" className="row">
+                    <Col span={4} ><img src={transportation} alt="transportation" /></Col>
+                    <Col span={4} ><img src={harvest} alt="harvest" /></Col>
+                    <Col span={4} ><img src={animals} alt="animals" /></Col>
+                    <Col span={4} ><img src={tshirt} alt="tshirt" /></Col>
+                  </Row>
+
+                  <Row justify="space-around" className="row">
+                    <Col className="row" span={4} ><img src={reduce} alt="reduce" /></Col>
+                    <Col span={4} ><img src={reuse} alt="reuse" /></Col>
+                    <Col span={4} ><img src={recycle} alt="recycle" /></Col>
+                    <Col span={4} ><img src={energy} alt="energy" /></Col>
+
+                  </Row>
+
+                  <Row justify="space-around" className="row">
+                    <Col span={4} ><img src={plants} alt="plants" /></Col>
+                    <Col span={4} ><img src={other} alt="other" /></Col>
+                    <Col span={4} ></Col>
+                    <Col span={4} ></Col>
+
+                  </Row>
+                </div>
               </div>
-              <div>
-                <Row justify="space-around" className="row">
-                  <Col span={4} ><img src={transportation} alt="transportation" /></Col>
-                  <Col span={4} ><img src={harvest} alt="harvest" /></Col>
-                  <Col span={4} ><img src={animals} alt="animals" /></Col>
-                  <Col span={4} ><img src={tshirt} alt="tshirt" /></Col>
-                </Row>
+            </Content>
 
-                <Row justify="space-around" className="row">
-                  <Col span={4} ><img src={reduce} alt="reduce" /></Col>
-                  <Col span={4} ><img src={reuse} alt="reuse" /></Col>
-                  <Col span={4} ><img src={recycle} alt="recycle" /></Col>
-                  <Col span={4} ><img src={energy} alt="energy" /></Col>
+            <Footer style={{ textAlign: 'center' }}>©2021 EcoGain Ltd.</Footer>
+          </Layout>
 
-                </Row>
-
-                <Row justify="space-around" className="row">
-                  <Col span={4} ><img src={plants} alt="plants" /></Col>
-                  <Col span={4} ><img src={other} alt="other" /></Col>
-                  <Col span={4} ></Col>
-                  <Col span={4} ></Col>
-
-                </Row>
-              </div>
-            </div>
-          </Content>
-
-          <Footer style={{ textAlign: 'center' }}>©2021 EcoGain Ltd.</Footer>
-        </Layout>
-
-        <Sider style={{ background: '#fff' }}>
-          <Menu defaultSelectedKeys={['Dashboard']} mode="inline">
-            <Menu.Item key='Dashboard'>
-              Categories
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed} style={{ background: '#fff' }}>
+            <Menu defaultSelectedKeys={['Dashboard']} mode="inline">
+              <Menu.Item key='Dashboard'>
+                Categories
               <Avatar style={{ backgroundColor: '#52BC3D', float: 'right' }} icon={<UserOutlined />} />
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
+              </Menu.Item>
+              <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+                <Menu.Item key="1">Option 1</Menu.Item>
+                <Menu.Item key="2">Option 2</Menu.Item>
 
-            </SubMenu>
-            <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="7">Option 7</Menu.Item>
-                <Menu.Item key="8">Option 8</Menu.Item>
               </SubMenu>
-            </SubMenu>
-            <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
+              <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="7">Option 7</Menu.Item>
+                  <Menu.Item key="8">Option 8</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu key="sub4" icon={<SettingOutlined />} title="Navigation Three">
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
 
-      </Layout>
-    </div >
-  );
+        </Layout>
+      </div >
+    );
+  }
 }
 
 export default App;
