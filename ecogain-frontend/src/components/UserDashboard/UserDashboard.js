cd558662ecce051ac35b9862356417b9211701ea
munishaboora <78922862+munishaboora@users.noreply.github.com>
2021-05-02 23:12:32 +0100

    Turned icon into buttons

--- ecogain-frontend/src/components/UserDashboard/UserDashboard.js
+++ ecogain-frontend/src/components/UserDashboard/UserDashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './UserDashboard.css';
 import { Layout, Avatar, Menu, Progress, Row, Col, Dropdown, Button } from 'antd';
-import { MenuOutlined, MenuUnfoldOutlined, MenuFoldOutlined, MailOutlined, SettingOutlined, AppstoreOutlined, StarTwoTone } from '@ant-design/icons';
+import { MenuOutlined, MenuUnfoldOutlined, MenuFoldOutlined, MailOutlined, SettingOutlined, AppstoreOutlined, StarTwoTone, LinkedinFilled, FacebookFilled, InstagramFilled, AndroidFilled, AppleFilled, WindowsFilled } from '@ant-design/icons';
 import Title from 'antd/lib/typography/Title';
 import SubMenu from 'antd/lib/menu/SubMenu';
 import transportation from '../Images/transportation.svg';
@@ -55,8 +55,8 @@ class App extends React.Component {
       <div className="App">
         <Layout>
           <Layout>
-            <Header className="site-layout-background" style={{ padding: 9, backgroundColor: '#449BCD' }} >
 
+            <Header className="site-layout-background" style={{ padding: 9, backgroundColor: '#449BCD' }} >
               <Title style={{ color: 'white' }} level={2}>
                 <h1 style={{ color: 'white', alignItems: "center" }}>
                   EcoGain
@@ -66,46 +66,45 @@ class App extends React.Component {
 
             <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
 
-
               <div className="site-layout-background" style={{ minHeight: 380 }}>
-                <div>
+                <div >
                   <StarTwoTone spin style={{ float: 'left', fontSize: '30px' }} /> <Progress percent={55} status="active" />
                 </div>
                 <div>
+
                   <Row justify="space-around" className="row">
-                    <Col span={4} ><img src={transportation} alt="transportation" /></Col>
-                    <Col span={4} ><img src={harvest} alt="harvest" /></Col>
-                    <Col span={4} ><img src={animals} alt="animals" /></Col>
-                    <Col span={4} ><img src={tshirt} alt="tshirt" /></Col>
+                    <button className="IconBtn"> <Col span={4} > <img src={transportation} alt="transportation" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4} > <img src={harvest} alt="harvest" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4} > <img src={animals} alt="animals" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4} > <img src={tshirt} alt="tshirt" /> </Col> </button>
                   </Row>
 
                   <Row justify="space-around" className="row">
-                    <Col className="row" span={4} ><img src={reduce} alt="reduce" /></Col>
-                    <Col span={4} ><img src={reuse} alt="reuse" /></Col>
-                    <Col span={4} ><img src={recycle} alt="recycle" /></Col>
-                    <Col span={4} ><img src={energy} alt="energy" /></Col>
-
+                    <button className="IconBtn"> <Col span={4}> <img src={reduce} alt="reduce" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4}> <img src={reuse} alt="reuse" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4}> <img src={recycle} alt="recycle" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4}> <img src={energy} alt="energy" /> </Col> </button>
                   </Row>
 
                   <Row justify="space-around" className="row">
-                    <Col span={4} ><img src={plants} alt="plants" /></Col>
-                    <Col span={4} ><img src={other} alt="other" /></Col>
+                    <button className="IconBtn"> <Col span={4}> <img src={plants} alt="plants" /> </Col> </button>
+                    <button className="IconBtn"> <Col span={4}> <img src={other} alt="other" /> </Col> </button>
                     <Col span={4} ></Col>
                     <Col span={4} ></Col>
-
                   </Row>
+
                 </div>
               </div>
             </Content>
 
-            <Footer style={{ textAlign: 'center' }}>©2021 EcoGain Ltd.</Footer>
+            <Footer style={{ textAlign: 'center', color: '#808080', fontSize: '20px' }}>©2021 EcoGain Ltd. <br /> <LinkedinFilled /> <FacebookFilled /> <InstagramFilled /> <AndroidFilled /> <AppleFilled /> <WindowsFilled /> </Footer>
           </Layout>
 
           <Sider trigger={null} collapsible collapsed={this.state.collapsed} style={{ background: '#fff' }}>
 
             <Menu defaultSelectedKeys={['Dashboard']} mode="inline">
               <Menu.Item key='Dashboard' >
-                {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                {React.createElement(this.state.collapsed ? MenuFoldOutlined : MenuUnfoldOutlined, {
                   className: 'trigger',
                   onClick: this.toggle
                 })}
