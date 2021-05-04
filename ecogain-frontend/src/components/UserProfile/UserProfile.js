b584470e21605977f46e9bd091d3a711cd2e489f
harriette <harrietteb@gmail.com>
2021-05-04 22:54:53 +0100

    connected to backend to display basic info for current user with token copied from backend

--- ecogain-frontend/src/components/UserProfile/UserProfile.js
+++ ecogain-frontend/src/components/UserProfile/UserProfile.js
@@ -6,14 +6,51 @@ import { Layout, Menu, Progress } from 'antd';
 import { UserOutlined, StarOutlined, MenuUnfoldOutlined, MenuFoldOutlined, LogoutOutlined, SettingOutlined, TableOutlined, StarTwoTone, LinkedinFilled, FacebookFilled, InstagramFilled, AndroidFilled, AppleFilled, WindowsFilled } from '@ant-design/icons';
 import Title from 'antd/lib/typography/Title';
 import SubMenu from 'antd/lib/menu/SubMenu';
+import { useState, useEffect } from 'react'
 const { Header, Footer, Sider, Content } = Layout;
 
 const UserProfile = () => {
     const { Header, Footer, Sider, Content } = Layout;
     const history = useHistory()
 
+    const [user, setUser ] = useState({})
+    const [fetched, setFetched] = useState(false)
+    
+
+    useEffect(() => {
+        const fetchFromAPI = async () => {
+          const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNjIwMTY2NTQ0fQ.BcQtrHfAfChkKZ0TXnyQ4FSi1gzfg3V-EDH25iPfua8'
+          const userFromServer = await fetchUser(token);
+
+          setUser(userFromServer);
+          if (user.user ) {
+            console.log(user.user)
+            console.log(user.user.name)
+            setFetched(true)
+          }
+          
+    };
+        fetchFromAPI();
+  }, []);
+
+  // write fetchTask func
+  const fetchUser = async (token) => {
+    const res = await fetch('http://localhost:5000/current_user', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+       "x-access-token": token,
+      },
+    });
+    const data = await res.json();
+    console.log(data)
+    return data;
+  };
+    
+
     return (
         <div className="App">
+            
             <Router>
                 <Layout>
                     <Layout>
@@ -32,7 +69,13 @@ const UserProfile = () => {
                                 <div >
                                     <StarTwoTone spin style={{ float: 'left', fontSize: '30px' }} /> <Progress percent={55} status="active" />
                                 </div>
+
+                                {fetched ? <>   <h1>{user.user.name}</h1> 
+                                            <h2>{user.user.username}</h2> 
+                                            <h5>{user.user.total_points}</h5> </> : ''} 
                             </div>
+
+                            
                         </Content>
 
                         <Footer style={{ textAlign: 'center', color: '#808080', fontSize: '20px' }}>©2021 EcoGain Ltd. <br /> <LinkedinFilled /> <FacebookFilled /> <InstagramFilled /> <AndroidFilled /> <AppleFilled /> <WindowsFilled /> </Footer>
